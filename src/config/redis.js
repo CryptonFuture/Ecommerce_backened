4b8abc1c66819546337cbd1a70eb23c8bef7ff25
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2026-08-04 16:30:24 +0500

    remove

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,8 +2,8 @@ const { createClient } = require("redis");
 
 const redisClient = createClient({
     socket: {
-        host: process.env.REDIS_HOST || "localhost",
-        port: process.env.REDIS_PORT || 6379,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
     },
 });
 
